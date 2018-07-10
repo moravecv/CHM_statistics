896085c7c85bb834da891be892f9e88b5893d57d
moravecv <moravec.vojtech@gmail.com>
2018-07-10 08:14:22 +0200

    canonical function

---
+++ canon.R
@@ -0,0 +1,26 @@
+library(candisc)
+
+tab <- readRDS("tab_chm_num.rds")
+
+############################### Canonical Function ##############################
+
+tab_lm <- lm(cbind(tarsus.lenght, tarsus.width, sternum, wing, tail, bill.lenght, 
+                    bill.width.i.f.of.nostrils, beak.nostril.to.peak, beak.height.i.f.of.nostrils, 
+                    width.of.scul.behind.the.eye, wing.patch.perc.DORS, wing.patch.perc.secondaries, 
+                    wattle.front.1, wattle.front.2) ~ origin, data = tab)
+
+tab_can <- candisc(tab_lm, data = tab)
+
+source("gg_candisc_plot.R")
+
+gg.candisc.plot(tab_can)
+
+###################### TAB Eigenvalues page 62 #################################
+
+cor <- tab_can$canrsq
+eig <- tab_can$eigenvalues[1:length(cor)]
+var <- eig/sum(eig) * 100
+cum <- cumsum(var)
+
+tab <- data.frame(Function = c(1:length(cor)), Eigenvalue = eig, Perc_of_variance = var, 
+                  Cumulative_perc = cum, Canonical_correlation = cor)
